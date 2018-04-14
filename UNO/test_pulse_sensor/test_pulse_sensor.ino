97e9772f6f523310abc1ae5881d72047b79382b0
necrosato <necrosato@live.com>
2018-04-14 14:26:19 -0700

    failed attempt to extrapolate a heartbeat

--- UNO/test_pulse_sensor/test_pulse_sensor.ino
+++ UNO/test_pulse_sensor/test_pulse_sensor.ino
@@ -3,6 +3,7 @@
 int PULSE_DATA = A0;
 int GRAPH_LENGTH = 127; // Midi max value
 int thresh = 520; //threshold for a hearbeat
+int rising_thresh = 15;
 
 void setup() {
     Serial.begin(115200);
@@ -10,15 +11,26 @@ void setup() {
     Serial.print("Setup Finished\n");
 }
 
-int signal;
+int signal, prev = 0;
+int diff, rise = 0;
 
 void loop() {
     // read the pulse sensors's ADC value
     signal = analogRead(PULSE_DATA);
+    diff = signal - prev;
+    prev = signal;
+    rise += diff;
+    if (abs(rise) > rising_thresh) {
+        //Serial.println("Heartbeat");
+        rise = 0;
+    }
+    Serial.println(signal);
+    //Serial.println(rise);
+    
+    /*
     // Map the 10 bit (0 - 1023) value to midi range (0 - 127)
     int signal_midi = map(signal, 0, 1023, 0, GRAPH_LENGTH);
     
-    /*
     // print a line graph
     Serial.print("<"); // starting end
     for (int i = 0; i < GRAPH_LENGTH; i++) {
@@ -31,7 +43,6 @@ void loop() {
     }
     Serial.print("> (" + String(signal) + ")\n");
     */
-    Serial.println(signal);
     delay(10);
 }
 
